dbe868c41a0ce72ae5abcdc08ed1503d21c89bf1
Ollie-bear <104463327+Ollie-bear@users.noreply.github.com>
2022-04-28 14:45:28 -0400

    Update standard_deviationv2.py

--- standard_deviationv2.py
+++ standard_deviationv2.py
@@ -4,9 +4,9 @@ input_string = input('Enter elements of a list separated by space:')
 print("\n")
 
 try:
-    
+    # this line of code makes the input numbers split in a list
     user_list = input_string.split()
-
+    # this line of code converts string in the user_list into interger so we can do math operations to it 
     new_list = [int(n) for n in user_list]
 
     print(statistics.pstdev(new_list))
